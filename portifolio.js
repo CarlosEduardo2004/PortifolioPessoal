a1f67609717c84535556c27eb8ee9ae7eb475ee2
CarlosEduardo2004 <108383230+CarlosEduardo2004@users.noreply.github.com>
2022-07-08 01:05:56 -0300

    Add files via upload

---
+++ portifolio.js
@@ -0,0 +1,7 @@
+let name = prompt("Olá, qual o seu nome?")
+let age = prompt("Sua idade")
+
+let infoConfirmation = confirm("Seu nome é: " +  name + "."
++ " Sua idade é: " + age + " anos")
+
+alert("Bem vindo, " + name + "." + " Você tá veio ein..." + age + "tão")
